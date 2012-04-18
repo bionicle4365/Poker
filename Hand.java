9ae52a156f5385e71d58f2c9b0677424f15b63f2
bionicle4365 <bionicle4365@gmail.com>
2012-04-18 17:10:20 -0300

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,5 +1,55 @@
+import java.util.*;
+
 public class Hand{
   
+  private Card[] hand;
+  public final int CARDS_IN_HAND = 5;
   
+  public Hand(Card[] hand){
+    
+    
+    
+  }
+  
+  public Card getCard(int index){
+    
+    
+    
+  }
+  
+  public void replace(int index, Card card){
+    
+    
+    
+  }
+  
+  public String toString(){
+    
+    
+    
+  }
+  
+  /**
+   * Counts the number of cards with each value in the hand
+   * @return tally array containing number of cards of each value from 2 to 14.
+	 */
+	public int[] getCounts() {
+	  int[] counts = new int[Card.HIGHEST_VALUE+1];
+		for (int i = 0; i < hand.length; i++) {
+			counts[hand[i].getValue()]++;
+		}
+		return counts;
+	}
+
+	/**
+	 * Creates a copy of the hand sorted first by value, then by suit
+	 * @return copy of the hand sorted first by value, then by suit
+	 */
+	public Card[] getSortedHand() {
+		Card[] sortedHand = Arrays.copyOf(hand, hand.length);
+		Arrays.sort(sortedHand);
+		return sortedHand;
+	}
+
   
 }
